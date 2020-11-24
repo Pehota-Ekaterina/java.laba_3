a8e72e7a4fc112b1126e37575cfff8cddf1dcec7
Pehota-Ekaterina <pexota02@list.ru>
2020-11-25 00:55:47 +0300

    добавлена в часть

--- src/GornerTableCellRenderer.java
+++ src/GornerTableCellRenderer.java
@@ -34,8 +34,33 @@ public class GornerTableCellRenderer implements TableCellRenderer {
         String formattedDouble = formatter.format(value);
         label.setText(formattedDouble);
 
+        if (col == 0) {
+            String[] val = formattedDouble.split("\\.");
+            int k = 0;
+            if(val.length > 1) {
+                k = val[1].length();
+            }
+
+            if(k<=3) {
+                panel.setBackground(Color.ORANGE);
+            } else {
+                panel.setBackground(Color.WHITE);
+            }
+            return panel;
+        }
+
         if (col==1 && needle!=null && needle.equals(formattedDouble)) {
             panel.setBackground(Color.RED);     //окрасить в красный
+        } else if(col == 1 ) {
+            String[] val = formattedDouble.split("\\.");
+            int k = 0;
+            if(val.length > 1) {
+                k = val[1].length();
+            }
+
+            if(k<=3) {
+                panel.setBackground(Color.ORANGE);
+            }
         } else {
             panel.setBackground(Color.WHITE);       //окрасить в белый
         }
